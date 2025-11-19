4e14fbd28d56323c1378c377943c3f0645009341
11249A418 <11249a418@kanchiuniv.ac.in>
2025-11-19 21:18:53 +0530

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
 using namespace std;
-
-// Define the Student class
 class Student 
 {
     private:
         string name;
         int rollno;
         float marks;
-
     public:
-        // Constructor to initialize the student object
-        Student(string n, int r, float m)
+       Student(string n, int r, float m)
          {
             name = n;
             rollno = r;
             marks = m;
         }
-
-        // Member function to display student information
-        void displayInfo()
+void displayInfo()
         {
             cout << "Student Name: " << name << endl;
             cout << "roll no: " << rollno << endl;
@@ -29,12 +23,9 @@ class Student
 
 int main()
  {
-    // Create a Student object
     Student student1("joy boy", 25, 90);
-
-    // Display student information
     student1.displayInfo();
-
     return 0;
 }
+
 
