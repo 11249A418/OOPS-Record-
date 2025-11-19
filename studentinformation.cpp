b1237b27b44df6dcc46f2c2b28aa42c97d4a4b62
11249A418 <11249a418@kanchiuniv.ac.in>
2025-11-19 21:21:20 +0530

    Update studentinformation.cpp

--- studentinformation.cpp
+++ studentinformation.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-
 class Student {
     int id;
     char name[50];
     int age;
     float marks;
-
 public:
     void input() {
         cout << "Enter student ID: ";
@@ -22,30 +20,24 @@ public:
         cout << "Enter student marks: ";
         cin >> marks;
     }
-
     void display() {
         cout << "ID: " << id << ", Name: " << name 
              << ", Age: " << age << ", Marks: " << marks << endl;
     }
 };
-
 int main() {
     int N;
-
     cout << "Enter number of students: ";
     cin >> N;
-
     Student students[N];  
-
     for (int i = 0; i < N; i++) {
         cout << "\nEnter details for student " << i + 1 << ":\n";
         students[i].input();
     }
-
     cout << "\nStudent Information:\n";
     for (int i = 0; i < N; i++) {
         students[i].display();
     }
-
     return 0;
 }
+
